7d4f514c1f03c6043b88483dc8ce05325d9d1805
kcharest <kalyn.charest@gmail.com>
2017-04-19 09:51:36 -0500

    Checkpoint 22

---
+++ landing.js
@@ -0,0 +1,15 @@
+ var animatePoints = function() {
+ 
+     var points = document.getElementsByClassName('point');
+     
+     var revealPoint = function(index) {
+         points[0].style.opacity = 1;
+         points[0].style.transform = "scaleX(1) translateY(0)";
+         points[0].style.msTransform = "scaleX(1) translateY(0)";
+         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+     }
+ 
+  for (var i = 0; i < points.length; i++) {
+      revealPoint(i);
+    }
+ };
