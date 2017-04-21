f4eb252e573e9bfd41ffa1ce240ab2209aea96e8
kcharest <kalyn.charest@gmail.com>
2017-04-21 09:11:02 -0500

    Checkpoint 23 rev1

--- landing.js
+++ landing.js
@@ -1,17 +1,15 @@
  var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {
-     
-     var revealPoint = function(index) {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
  
-  for (var i = 0; i < points.length; i++) {
-      revealPoint(i);
-    }
+ var revealPoint = function(point) {
+         point.style.opacity = 1;
+         point.style.transform = "scaleX(1) translateY(0)";
+         point.style.msTransform = "scaleX(1) translateY(0)";
+         point.style.WebkitTransform = "scaleX(1) translateY(0)";
+ };
+
+ var animatePoints = function(points) { 
+     forEach(points, revealPoint);
  };
 
  window.onload = function() {
@@ -20,12 +18,11 @@
          animatePoints(pointsArray);
      }
      
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
      
-      window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+     window.addEventListener("scroll", function(event) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
              animatePoints(pointsArray);  
         }
      });
  }
+ 
